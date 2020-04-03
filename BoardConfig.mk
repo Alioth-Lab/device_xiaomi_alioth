b522914e53041ca54faac619322035dc8307f8b3
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-04-03 18:56:35 +0200

    alioth: Set linker to LLD

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ARCH := arm64
+KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := r445002
 TARGET_KERNEL_CONFIG := alioth_defconfig
