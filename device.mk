c2d5d47ce71c4e125c6eade6728c17159c7ccc30
althafvly <althafvly@gmail.com>
2022-02-22 21:51:43 +0100

    alioth: touch: add IHighTouchPollingRate
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- device.mk
+++ device.mk
@@ -593,6 +593,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_kona
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
